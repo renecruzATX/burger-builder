44271ca08139015d9d87107343c7e05bc0a153f5
Rene Cruz <renecruzatx@outlook.com>
2018-11-08 21:43:26 -0600

    Added Navigation Items to Toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
 
 
 const toolbar = (props) => (
     <header className={styles.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo/>
         <nav>
             <ul>
-                ...
+                <NavigationItems/>
             </ul>
         </nav>
     </header>
