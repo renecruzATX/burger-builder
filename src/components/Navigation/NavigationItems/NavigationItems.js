b03d9d5cdc824adcf4134965a09b0b6d115e169c
Rene Cruz <renecruzatx@outlook.com>
2018-12-05 15:44:06 -0600

    Navigation done

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = (props) => (
+const navigationItems = () => (
     <ul className={styles.NavigationItems}>
         <NavigationItem link="/" active>Burger Builder</NavigationItem>
         <NavigationItem link="/">Checkout</NavigationItem>
